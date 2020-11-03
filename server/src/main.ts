5928ca0987e4950b310ad0c74f9b35e1abf59d66
mahmoudAcm <mahmoud.acm.123@gamil.com>
2020-11-03 06:05:23 +0200

    enabling cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowList = ["http://localhost:3000"];
+  app.enableCors({
+    origin: allowList
+  });
   await app.listen(3001);
 }
 bootstrap();
